a9dc0a764dba97b3340c197a288e49932eb7cde2
hj <day_1111@126.com>
2020-07-09 15:19:33 +0800

    自动填充优化

--- src/main/java/com/example/component/MyMetaObjectHandler.java
+++ src/main/java/com/example/component/MyMetaObjectHandler.java
@@ -9,28 +9,49 @@ package com.example.component;/**
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- *@description:
+ *@description: 默认填充字段类
  *@author: huangJian
  *@create: 2020-07-09
  */
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
+    /*
+    如果要修改创建人，修改人， 可以从request.Context.XXX中获取登录人
+
+     */
+
+
     @Override
     public void insertFill(MetaObject metaObject) {
         System.out.println("insertFill ~~");
         //setInsertFieldValByName(); 这个方法已过时
-        setFieldValByName("createTime",new Date(),metaObject);
+
+        //判断属性是否有set createTime 才执行
+        if(metaObject.hasSetter("createTime")){
+            setFieldValByName("createTime",new Date(),metaObject);
+        }
+
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        System.out.println("updateFill ~~");
         //setUpdateFieldValByName() 这个方法已过时
-        setFieldValByName("updateTime",new Date(),metaObject);
+        //判断属性是否有set updateTime 才执行
+        if(metaObject.hasSetter("updateTime")){
+            //如果属性上没有值再默认填充  有值就不再填充默认值
+            Object val = getFieldValByName("updateTime", metaObject);
+            if(StringUtils.isEmpty(val)){
+                System.out.println("updateFill ~~");
+                setFieldValByName("updateTime",new Date(),metaObject);
+            }
+
+        }
+
     }
 }
